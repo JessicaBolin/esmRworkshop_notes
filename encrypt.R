4ad52404b15f89ed5ff424b138d1e1b03bc2c163
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2025-05-28 11:22:01 -0700

    Update workshop notes

---
+++ encrypt.R
@@ -0,0 +1,10 @@
+staticryptR::staticryptr(
+  files = "_output/index.html",
+  directory = "_output",
+  password = "redabalone",
+  short = TRUE,
+  recursive = TRUE,
+  template_instructions = "Enter the password or contact jabbolin@ucdavis.com",
+  template_title = "Workshop notes - R for marine ecologists: wrangling Earth System Model outputs",
+  template_button = "Access"
+)
