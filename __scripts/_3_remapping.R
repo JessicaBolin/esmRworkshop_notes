1b928f9a381d8c75663b49db77c13ffba579ed61
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2025-06-02 11:57:58 -0700

    Update

--- __scripts/_3_remapping.R
+++ __scripts/_3_remapping.R
@@ -15,7 +15,9 @@ source(paste0(getwd(), "/__scripts/helpers.R"))
 ### R
 rr <- rast(paste0(pth, cmip_pth, 
                   "/tos_Omon_ACCESS-CM2_historical_r1i1p1f1_gn_185001-201412.nc")) 
+rr
 rr <- rr[[1]] #the first layer
+rr
 plot(rr) # Nope! 
 
 
@@ -23,19 +25,27 @@ plot(rr) # Nope!
 
 rr
 
-nc <- nc_open(paste0(pth, cmip_pth, 
+nc <- ncdf4::nc_open(paste0(pth, cmip_pth, 
                      "/tos_Omon_ACCESS-CM2_historical_r1i1p1f1_gn_185001-201412.nc"))
 nc
 
 
 # 3.2.2 Remap with CDO and R ----------------------------------------------------
 
+
 remap_n_crop_temp <- function(nc_file,
-                              cell_res = 0.25, 
-                              infold = paste0(pth, cmip_pth), 
-                              outfold = paste0(pth, cmip_pth_proc),
+                              cell_res = 0.25, #change this at will
+                              infold = paste0(pth, cmip_pth),  #where raw ESMs live
+                              outfold = paste0(pth, cmip_pth_proc), #where remapped ESMs will be stored
                               xmin = -126, xmax = -115, ymin = 32, ymax = 43) {
   
+  # 4 steps for CDO command: cdo -operator inputfile outputfile
+  # IN this case, we have three operators. 
+  # -L = low memory mode
+  # -sellonlatbox = select geographic box
+  # -remapbil,r = regrid data to regular grid with bilinear interpolation
+  #   # estimates new grid cell vlues by using the four nearest input grid points
+  # numbers = target grid that we're interpolating into (rows and columns required)
   system(paste0("cdo -L -sellonlatbox,", xmin, ",", xmax, ",", ymin, ",", ymax,  
                 " -remapbil,r", 360*(1/cell_res), "x", 180*(1/cell_res), 
                 " -select,name=tos ", infold, "/", nc_file, " ", outfold, "/", nc_file))  
--- __scripts/_4_OISST.R
+++ __scripts/_4_OISST.R
@@ -17,52 +17,65 @@
 # Dependencies ------------------------------------------------------------
 
 source(paste0(getwd(), "/__scripts/helpers.R"))
-oFold <- paste0(pth, oisst_pth, "/") # Set destination folder
+#oFold <- paste0(pth, oisst_pth, "/") # Set destination folder where raw OISST will be stored
+oFold <- "/Users/admin/Desktop/test/oisstraw/"
 url <- "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/v2.1/access/avhrr" # Set base URL
 
 # Scrape file names from HTML ---------------------------------------------
 
 # Get the links that appear there as yyyymm dates
 pg <- read_html(url) # Read the HTML (takes a while)
+pg
 sFld <- html_attr(html_nodes(pg, "a"), "href") %>%  # Extract the links
   grep("\\d\\d\\d\\d\\d\\d", ., value = TRUE) # Just the folders with 6 digits (i.e., one per month)
+sFld
 
-# Check what files might already be in the folder
+# Check what files might already be in the output folder
 f <- dir(oFold, pattern = ".nc")
+f
 
 # Extract identifiers from the file names - works at level of month
 ncs <- gsub("oisst-avhrr-v02r01.", "", f) %>%
   gsub("\\d\\d\\.nc", "", .) %>% 
   unique() %>% 
   paste0(., "/")
+ncs
 
 # Exclude files (months) that are already downlaoded
 sFld <- sFld[!sFld %in% ncs]
+sFld
 
 # Function to download OISST ----------------------------------------------
+
+#yrstrt = 1995
+#yrend = 1996
+#i = sFld[1]
 
 download_oisst <- function(yrstrt, yrend) {
   
   # Subset sFld to only include time period of interest
-  ind1 <- grep(yrstrt, sFld) %>% min 
+  ind1 <- grep(yrstrt, sFld) %>% min #index of first year in html parsed vector
   ind2 <- grep(yrend, sFld) %>% max
   sFld <- sFld[ind1:ind2] 
-  length(sFld) 
+  length(sFld) #number of months  
   
-  for(i in sFld) {
+  for(i in sFld) { #for each year-month combo, read in the links
     urli <- paste0(url, "/", i)
     pgi <- read_html(urli) # Read the HTML, can take a while
     sFldi <- html_attr(html_nodes(pgi, "a"), "href") %>%  # Extract the links
       grep(".nc", ., value = TRUE) # Just the netCDFs
-    for(j in sFldi) {
+    for(j in sFldi) { #download each daily netCDF
       download.file(paste0(urli, j), paste0(oFold, j))
     }
   }
 }
 
-tic(); download_oisst(yrstrt = 1995, yrend = 2014); toc()
+#tic(); download_oisst(yrstrt = 1995, yrend = 2014); toc()
+tic(); download_oisst(yrstrt = 1995, yrend = 1996); toc()
 
 # 4.2 Preprocess OISST (i.e., remap) ------------------------------------------------
+
+
 
 oisst_mr <- function(yr,
                      infile = paste0(pth, oisst_pth),
@@ -108,6 +121,11 @@ tic(); future_map(years, ~ oisst_mr(.x)); toc()
 plan(sequential) # Return to sequential processing
 # Takes ~1 minute for Jessie! Nice. 
 
+# Or, for loop version
+# for (i in years) {
+#   oisst_mr(i)
+# }
+
 list.files(paste0(pth, oisst_pth_proc))
 
 # 4.3 Visualise ---------------------------------------------------------------
@@ -117,6 +135,6 @@ oisst <- terra::rast(paste0(pth, oisst_pth_proc,
 esm <- terra::rast(paste0(pth, cmip_pth_proc, 
                           "/tos_Omon_ACCESS-CM2_historical_r1i1p1f1_gn_185001-201412.nc"))[[1]]
 terra::plot(oisst, main = "OISST Jan 1995")
-maps::map("world", add = T)
+maps::map("state", add = T)
 terra::plot(esm, main = "ESM Jan 1850")
-maps::map("world", add = T)
+maps::map("state", add = T)
